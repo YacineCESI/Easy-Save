cdb10459dc41aa47dcc4aefea318d33babe9f92b
YacineCESI <155562563+YacineCESI@users.noreply.github.com>
2025-05-19 15:45:52 +0200

    Add files via upload

---
+++ EasySaveV1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySave.View;
+using EasySave.ViewModel;
+
+namespace EasySaveV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var mainViewModel = new MainViewModel();
+
+
+            var consoleInterface = new ConsoleInterface(mainViewModel);
+
+
+            consoleInterface.Start();
+        }
+    }
+}
