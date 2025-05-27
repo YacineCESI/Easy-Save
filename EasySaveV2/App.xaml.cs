bce35fdbd47365b1c8d2d3d7d4dad13eda11ccb4
ycine <yacine.bouzerara@viacesi.fr>
2025-05-27 07:53:55 +0200

    Single-instance application

--- EasySaveV2/App.xaml.cs
+++ EasySaveV2/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
+using System.Threading;
 using System.Windows;
 
 namespace EasySaveV2
@@ -9,6 +11,30 @@ namespace EasySaveV2
     /// </summary>
     public partial class App : Application
     {
-    }
+        private static Mutex _singleInstanceMutex;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "EasySaveV2_SingleInstanceMutex";
+            bool createdNew;
+
+            _singleInstanceMutex = new Mutex(true, mutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("EasySaveV2 is already running.", "Single Instance", MessageBoxButton.OK, MessageBoxImage.Information);
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _singleInstanceMutex?.ReleaseMutex();
+            _singleInstanceMutex?.Dispose();
+            base.OnExit(e);
+        }
+    }
 }
